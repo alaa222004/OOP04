86214df92bf02755d34e5193391f8513595b4d00
alaa samer <alaasamer222004@gmail.com>
2025-07-16 21:18:57 +0300

    Q1 solved

--- OOP04/Program.cs
+++ OOP04/Program.cs
@@ -4,9 +4,20 @@ namespace OOP04
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
-        }
+            #region Q1
+            //            Question 1:
+            //What is the primary purpose of an interface in C#?
+            //a) To provide a way to implement multiple inheritance
+            //b) To define a blueprint for a class
+            //c) To declare abstract methods and properties
+            //d) To create instances of objects
+
+            //ANS:(C)
+
+            #endregion
+
     }
+}
 }
