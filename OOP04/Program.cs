b0e6296df8f0e0955e5ba5b3f6b248321b403249
alaa samer <alaasamer222004@gmail.com>
2025-07-16 21:23:04 +0300

    Q3 solved

--- OOP04/Program.cs
+++ OOP04/Program.cs
@@ -6,29 +6,39 @@ namespace OOP04
     {
         static void Main()
         {
-            #region Q1
-            //            Question 1:
-            //What is the primary purpose of an interface in C#?
-            //a) To provide a way to implement multiple inheritance
-            //b) To define a blueprint for a class
-            //c) To declare abstract methods and properties
-            //d) To create instances of objects
+        #region Q1
+        //            Question 1:
+        //What is the primary purpose of an interface in C#?
+        //a) To provide a way to implement multiple inheritance
+        //b) To define a blueprint for a class
+        //c) To declare abstract methods and properties
+        //d) To create instances of objects
 
-            //ANS:(A)
+        //ANS:(A)
 
-            #endregion
-            #region Q2
+        #endregion
+        #region Q2
 
 
-            //Which of the following is NOT a valid access modifier for interface members in C#?
-            //a) private
-            //b) protected
-            //c) internal
-            //d) public
+        //Which of the following is NOT a valid access modifier for interface members in C#?
+        //a) private
+        //b) protected
+        //c) internal
+        //d) public
 
-            //ANS:(A)
+        //ANS:(A)
 
+        #endregion
+        #region Q3
+        //            Question 3:
+        //Can an interface contain fields in C#?
+        //a) Yes
+        //b) No
+        //c) Only if they are static
+        //d) Only if they are read only
+
+            //ANS: (B)
             #endregion
         }
-    }
+}
 }
